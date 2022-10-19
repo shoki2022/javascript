10905895af8d75dc60000c1d09fe160506b9a56c
shoki2022 <113892000+shoki2022@users.noreply.github.com>
2022-10-20 01:09:06 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+for (let number=1; number<=50; number++){
+  if(number%10===0){
+    console.log("今"+number+"回ループしました")
+  }
+  else if(number%4===0){
+    console.log("４で割れる数です"+number)
+  }
+  else if(number===50)
+  alert("50回カウントが終わりました。")
+
+  else{
+    console.log(number)
+  }
+}
