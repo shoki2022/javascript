1f76bf19d917c3a11e57c4935a641672ffa9192d
shoki2022 <113892000+shoki2022@users.noreply.github.com>
2022-10-20 02:08:37 +0900

    kadai

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ for (let number=1; number<=50; number++){
   if(number%10===0){
     console.log("今"+number+"回ループしました")
   }
-  else if(number%4===0){
+  
+  if(number%4===0){
     console.log("４で割れる数です"+number)
   }
   else if(number===50)
